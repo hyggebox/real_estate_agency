6f40695acb03bb347f792780f2cbddf15c5987be
Anastasia <asya_apto@mail.ru>
2022-01-18 00:00:26 +0300

    Display phonenumbers in main admin panel

--- property/admin.py
+++ property/admin.py
@@ -5,7 +5,8 @@ from .models import Flat, Complaint
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
+    list_display = ('address', 'price', 'new_building', 'construction_year',
+                    'town', 'owners_phonenumber', 'valid_owners_phonenumber')
     list_editable = ['new_building']
     readonly_fields = ['created_at']
     list_filter = ['new_building', 'rooms_number', 'has_balcony', 'active']
