dbc1a548c835345055fae5f18d2b88ac52517e1f
Anastasia <asya_apto@mail.ru>
2022-01-17 23:45:36 +0300

    Add Flat.valid_owners_phonenumber field

--- property/models.py
+++ property/models.py
@@ -1,3 +1,5 @@
+from phonenumber_field.modelfields import PhoneNumberField
+
 from django.db import models
 from django.utils import timezone
 
@@ -7,6 +9,10 @@ from django.contrib.auth.models import User
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    valid_owners_phonenumber = PhoneNumberField(
+        'Нормализованный номер владельца',
+        null=True
+    )
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
