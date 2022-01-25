c384ecb977a655f7189f8e7c9b8c145419dc9a24
Anastasia <asya_apto@mail.ru>
2022-01-25 23:26:48 +0300

    Load owners to Flat page

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Flat.owners.through
+    extra = 3
+    raw_id_fields = ('owner', 'flat',)
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address']
     list_display = ('address', 'price', 'new_building', 'construction_year',
@@ -11,6 +17,7 @@ class FlatAdmin(admin.ModelAdmin):
     readonly_fields = ['created_at']
     list_filter = ['new_building', 'rooms_number', 'has_balcony', 'active']
     raw_id_fields = ('liked_by',)
+    inlines = [OwnerInline]
 
 
 class ComplaintAdmin(admin.ModelAdmin):
